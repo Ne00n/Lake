f9af31966ce8d3ef350ae621024e6917ec4d06d3
Ne00n <neo@tiefkuehler-madness.me>
2017-05-23 20:23:59 +0200

    Updated Tests

--- tests/Main.php
+++ tests/Main.php
@@ -14,21 +14,28 @@
       }
     }
 
+    private function validateStatus() {
+      if ($this->Lake->getSuccess() === false) { return false; }
+      if (!empty($this->Lake->getErrors())) { return false; }
+      return true;
+    }
+
     public function testComponents() {
       //INSERT
       $insertID = $this->Lake->INSERT('Users')->INTO(array('Name' => 'Test'))->VAR('s')->DONE();
       $this->assertEquals($insertID,1);
-      $this->assertEquals($this->Lake->getErrors(),array());
+      $this->assertEquals($this->validateStatus(),true);
       //SELECT
       $results = $this->Lake->SELECT(array('ID','Name'))->FROM('Users')->WHERE(array('ID' => $insertID))->OR()->WHERE(array('Name' => 'Test'))->VAR('is')->DONE();
       $this->assertEquals($results,array(0 => array('ID' => 1,'Name' => 'Test')));
-      $this->assertEquals($this->Lake->getErrors(),array());
+      $this->assertEquals($this->validateStatus(),true);
       //DELETE
       $this->Lake->DELETE()->FROM('Users')->WHERE(array('ID' => $insertID))->VAR('i')->DONE();
-      $this->assertEquals($this->Lake->getErrors(),array());
+      $this->assertEquals($this->validateStatus(),true);
       //Check if nothing is left
       $results = $this->Lake->SELECT(array('ID'))->FROM('Users')->WHERE(array('ID' => $insertID))->VAR('i')->DONE();
       $this->assertEquals($results,array());
+      $this->assertEquals($this->validateStatus(),true);
     }
 
   }
